a6dadccc04a2c4a10b7e61da34998100d5677ee5
kevin <kevinkibet942@gmail.com>
2024-04-02 11:29:20 +0300

    feat: modified index.js

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // code your solution here
+const record = [
+    { year: "2018", result: "N/A"},
+    { year: "2017", result: "N/A"},
+    { year: "2016", result: "N/A"},
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    { year: "2012", result: "N/A"},
+    { year: "2011", result: "N/A"},
+    { year: "2010", result: "N/A"},
+    { year: "2009", result: "N/A"},
+    { year: "2008", result: "N/A"},
+    { year: "2007", result: "N/A"},
+    { year: "2006", result: "N/A"},
+    { year: "2005", result: "W"},
+
+  ]
+function superbowlWin(record){
+    const win = record.find(item => item.result === "W");
+    if(win){
+        return win.year;
+    }
+    else{return undefined}
+}
+
+const celebration = superbowlWin(record);
+console.log(celebration)
